83f9ded841103861c970aee769759f2e7fed7c53
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-09 09:48:41 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'a'; n = 'z'
+
+	while (i <= 'A'; n <= 'Z')
+	{
+		putchar(i);
+		i++;
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
