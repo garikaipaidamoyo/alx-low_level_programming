b9623433745401e1239dab77b82535cdf748ac85
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-09 17:12:52 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int w = '0';
+
+	while (w <= '9');
+	{
+		w++;
+		putchar (w);
+
+		if (w != '9')
+		{
+			putchar (' ');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
