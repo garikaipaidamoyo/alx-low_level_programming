bebb6be6b38997e2ddf4a9e564db9ebd32f84ea6
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-13 09:44:05 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h" 
 
 /**
@@ -7,7 +8,8 @@
  * Return: Always 0.
  */
 
-int _putchar(char c)
+int _putchar(char c);
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
