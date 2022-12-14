63d2f9afd444cade265ed00da15d921d89c42104
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-14 07:06:41 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hrs, min;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
