70786ea133d0e1fa956f4868cd21e6af3a925f8b
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-14 07:39:53 -0800

    learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,16 +5,16 @@
  */
 void times_table(void)
 {
-	int i = 0;
+	int i = 0, n = 0, result;
 
 	for (i = 0; i <= 9; i++)
 	{
-		int n = 0;
-
 		for (n = 0; n <= 9; n++)
 		{
+			result = i * n;
 			_putchar(n);
 			_putchar(' ');
+			_putchar(',');
 		}
 		_putchar(i);
 		_putchar('\n');
