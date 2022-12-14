6a1a19bc9b39c09592d25e8e6ad3633ee25da50e
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-14 09:39:28 -0800

    Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from integer n to 98
+ * @n: the integer to print
+ *
+ */
+
+void print_to_98(int n)
+{
+	int n = 0;
+
+	for (n = 0; n <= 98; n++)
+	{
+		printf("%d", n);
+		printf(", ");
+		printf("\n");
+	}
+}
+
