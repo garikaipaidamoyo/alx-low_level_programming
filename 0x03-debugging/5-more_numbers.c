d2ffd53468449e45270c1a4266468136efdc73fc
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-15 10:55:50 -0800

    Numbers constitute the only universal language

---
+++ 0x03-debugging/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * more_numbers - prints 10 times the integers
+ */
+
+void more_numbers(void)
+{
+	for (i = '0'; i <= '14'; i++)
+	{
+		_putchar(i);
+		i *= 10;
+
+	}
+	_putchar('\n')
