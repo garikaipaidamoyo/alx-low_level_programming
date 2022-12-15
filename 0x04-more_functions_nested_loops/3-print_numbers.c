faf185762b11824dcbe68ec2f9fb41ea9cb8a921
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-15 09:14:57 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,15 @@
 /**
  * print_numbers - prints the integer
  *
- * Return: Always 0.
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
