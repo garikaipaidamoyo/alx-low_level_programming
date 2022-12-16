e3b20563b89bc566f56a75e7af07354e2dc1897b
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-16 03:19:00 -0800

    The shortest distance between two points is a straight line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: The integer to print
+ *
+ * Return: Always 0
+ * 
+ */
+
+void print_square(int size)
+{
+	int size;
+
+	char #;
+
+	if (size <= '0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
