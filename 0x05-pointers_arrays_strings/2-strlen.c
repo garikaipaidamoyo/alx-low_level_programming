843a0b8c18c0c5acaf8e72cb2d1a61fd156a1aca
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-20 04:58:37 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: The character to prints
+ *
+ * Return: Always 0 .
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = '0' ; S(i) != '\0'; i++)
+	{
+		return (i);
+	}
+}
+
