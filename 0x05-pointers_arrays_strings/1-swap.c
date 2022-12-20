5f24f53b9293e920500ea1df94f3638af9f692ab
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-20 03:11:47 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
