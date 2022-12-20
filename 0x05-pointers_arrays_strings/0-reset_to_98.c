5afe2f186dc0df194d62d441f0ac2cbb2b687b7d
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-20 01:38:06 -0800

    Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an integer as parameter and updates the value it points to 98.
+ *
+ * Return: Always 0 .
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	
+}
