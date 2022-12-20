2cc0480cdc72279a1569bc5b7365e738f7dfe492
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-20 07:51:30 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the character to print
+ *
+ * Return: Always 0 .
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i])
+	}
+	_putchar('\n');
+}
