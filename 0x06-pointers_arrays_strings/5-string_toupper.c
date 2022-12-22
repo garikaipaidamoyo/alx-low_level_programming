52e8c11f09c6415427131af3983f9c7a5aff80fc
paidamoyo <garikaipaidamoyo@gmail.com>
2022-12-22 08:37:39 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @k: The character to print
+ *
+ * Returns: Always 0 .
+ */
+
+char *string_toupper(char *k)
+{
+	int i = 0;
+
+	while (k[i] != '\0')
+		if (k[i] > 96 && k[i] < 123;)
+			k[i] -= 96 -90;
+}
