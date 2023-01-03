3f799a5f18fa4068c315560cebbce93547712a01
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-03 13:19:48 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,16 +12,16 @@ include "main.h"
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int counter;
+	unsigned int i;
 
-	for (; *s != '\0'; s++)
+	while (*s != '\0')
 	{
-		for (counter = 0; accept[counter] != '\0'; counter++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
-
-			if (*s == accept[counter])
+			if (*s == accept[i])
 				return (s);
 		}
+		s++;
 	}
 	return (0);
 }
