9fc2b218c8010e3e506b209b631d5ed3a73a39da
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-03 08:02:34 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
-#endif /* MAIN.H */
+#endif /* MAIN_H */
