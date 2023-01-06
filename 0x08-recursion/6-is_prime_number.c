6846d1f85d1cc2404d9a889f952af38fe1367d18
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-06 04:10:47 -0800

    Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * checker - returns a prime number
+ * @n: prime number
+ * @base: base
+ *
+ * Returns: 1 on success
+ * Otherwise 0 on failer
+ */
+
+int checker(int n, int base)
+{
+	if (base % n == 0 || base < 2)
+		return (0);
+	else if (n == base - 1)
+		return (1);
+	else if (base > n)
+		return (checker(n + 1, base));
+	return (1);
+}
+/**
+ * is_prime_number - returns prime number
+ * @n: prime number
+ */
+
+int is_prime_number(int n)
+{
+	return (checker(2, n));
+}
