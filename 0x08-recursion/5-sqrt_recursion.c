ede4e6c7156e0c25aaa814ee5d929fe8d231fbf5
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-06 03:35:59 -0800

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * runner - returns the natural square root of a number
+ * @n: power
+ * @base: base 
+ *
+ * Return: natural square root
+ */
+
+int runner(int n, int base)
+{
+	if (n * n == base)
+		return (n);
+	if (n * n > base)
+		return (-1);
+	return (runner((n+1), base));
+/**
+ * _sqrt_recursion - returns the natural square root
+ * @n: power
+ *
+ * Return: natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (runner(1,n));
+}
