a265b776a1719cd80c2a5f12bf65f02036e82d12
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-10 05:04:03 -0800

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,8 +6,8 @@
  * @size: The size of the array to be initialized.
  * @c: The specific char to intialize the array with.
  *
- * Return: If size == 0 or the function fails - NULL.
- *         Otherwise - a pointer to the array.
+ * Return: NULL
+ * Otherwise - a pointer to the array.
  */
 char *create_array(unsigned int size, char c)
 {
