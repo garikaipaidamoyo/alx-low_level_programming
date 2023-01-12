33970e3cdfa2fb921d11f49d3c079d86d6231b7d
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-12 06:06:24 -0800

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,8 +17,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
-		if (p == NULL)
-			return (NULL);
+	if (p == NULL)
+		return (NULL);
 	for(i = 0; i < nmemb; i++)
 		p[i] = 0;
 	return (p);
