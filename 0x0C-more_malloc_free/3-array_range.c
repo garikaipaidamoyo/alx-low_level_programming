65ce0318a9399885155382c6e84d7a8e93922aeb
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-12 07:26:03 -0800

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,17 +8,19 @@
  *
  * Return: the pointer to the newly created array
  */
-int *array_range(int min, int max) {
-    if (min > max) {
-        return NULL;
-    }
-    int size = max - min + 1;
-    int *arr = malloc(sizeof(int) * size);
-    if (arr == NULL) {
-        return NULL;
-    }
-    for (int i = 0; i < size; i++) {
-        arr[i] = min + i;
-    }
-    return arr;
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	int size = max - min + 1;
+	p = malloc(sizeof(int) * size);
+
+	if (p == NULL)
+		return (NULL);
+	for(i = min; i <= max; i++)
+		return (p);
 }
