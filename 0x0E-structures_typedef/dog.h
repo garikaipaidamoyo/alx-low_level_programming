dffbd22158518fa42a4a3d46c54c073706bf2cf9
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-16 12:56:53 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * @char - name of the dog
+ * @float - age of the dog 
+ * @char -  second char, owner of the dog
+ */ 
 struct dog
 {
 	char *name;
@@ -8,4 +13,4 @@ struct dog
 	char *owner;
 };
 
-#endif 
+#endif
