cf9702b3719bfa973ce66a27e5ad6b612674bf94
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-18 06:30:23 -0800

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL || cmp == NULL || size <= 0)
+	if (array == NULL && cmp == NULL && size <= 0)
 	{
 		return (-1);
 	}
 	for (int i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
-		{
-			return (i);
-		}
+		cmp(array[i];
+		return (i);
 	}
 	return (-1);
 }
