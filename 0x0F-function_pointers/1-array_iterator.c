9de4539cea93d75608c98b54e1ad38b0803dac4e
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-21 09:09:45 -0800

    What's my name

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int x))
 
 	if (array != NULL && action != NULL)
 	{
-		for (i = 0U; i < size; ++i)
+		for (i = 0; i < size; ++i)
 			action(array[i]);
 	}
 }
