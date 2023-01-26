64d48de9917a4656704b5d2e49728ad69a0a0a26
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-26 08:33:06 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,9 +6,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
@@ -16,11 +16,11 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		}
-		h = h->next;
 		count++;
+		h = h->next;
 	}
-
 	return (count);
+	
 }
