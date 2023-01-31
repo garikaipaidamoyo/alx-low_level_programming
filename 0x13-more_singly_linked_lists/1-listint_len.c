1e8e6acd6dca8e7d6cb15e9346a6f2a0c444473b
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-31 04:53:14 -0800

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
 /**
  * listint_len - returns the number of elements in a linked listint_t list
  * @h: pointer to the linked list
- * 
+ *
  * Return: the number of elements in a linked listint_t list
  */
 
