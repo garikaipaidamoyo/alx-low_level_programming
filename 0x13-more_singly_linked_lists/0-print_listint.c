3bb6ff5483f0210dc909df02ab60b920eda526f5
paidamoyo <garikaipaidamoyo@gmail.com>
2023-01-31 06:43:23 -0800

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: the list
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
  *
- * Return: number of nodes
+ * Return: number of elements in the list
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
