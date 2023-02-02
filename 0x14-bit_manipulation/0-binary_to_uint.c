983b542062a0fa346e46e278c94bcee074fde817
paidamoyo <garikaipaidamoyo@gmail.com>
2023-02-02 09:26:03 -0800

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,11 +9,11 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int bit = 0;
+
 	while (b && *b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		
 		bit = bit << 1;
 		bit = bit | (*b - '0');
 		b++;
