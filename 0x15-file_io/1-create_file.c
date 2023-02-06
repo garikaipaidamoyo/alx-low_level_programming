110f5133a4253c174eda808fd7490b5002433545
paidamoyo <garikaipaidamoyo@gmail.com>
2023-02-06 10:00:43 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  * create_file - ...
-  * @filename: ...
-  * @text_content: ...
+  * create_file - creates a file
+  * @filename: the name of te file to create
+  * @text_content: is a NULL terminated string to write to the file
   *
-  * Return: ...
+  * Return: 1 on success, -1 on failure
   */
 int create_file(const char *filename, char *text_content)
 {
